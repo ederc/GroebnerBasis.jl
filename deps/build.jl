633ad202b511d28d91af1739c448c9dd5da7113e
Christian Eder <ederc@mathematik.uni-kl.de>
2018-08-02 17:37:10 +0200

    changes to new gb branch

--- deps/build.jl
+++ deps/build.jl
@@ -1,7 +1,8 @@
 oldwdir = pwd()
 
 # at the moment we only use the experimental plain branch of gb
-@show GB_BRANCH = "hashing-structs"
+@show GB_BRANCH = "only-hash-data"
+ #@show GB_BRANCH = "hashing-structs"
  # @show GB_BRANCH = "structured-polynomials"
 
 pkgdir = dirname(dirname(@__FILE__))
