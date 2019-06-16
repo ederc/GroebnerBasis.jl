c997fb7d6431c933630a6bc02af9dd2e902fdc9b
Christian Eder <ederc@mathematik.uni-kl.de>
2019-06-16 18:12:48 +0200

    faster new polys generator due to less tuples and less inner loops

--- src/ZnZ.jl
+++ src/ZnZ.jl
@@ -179,7 +179,8 @@ function _recombine(Ga, Gb; timings = Dict())
   push!(Gagenslifted, S(a))
   push!(Gbgenslifted, S(b))
 
-  lt = Vector{Tuple{Int, Vector{Int}}}()
+#lt = Vector{Tuple{Int, Vector{Int}}}()
+  ltp = Vector{Tuple{Tuple{Int, Int}, Int, Vector{Int}}}()
 
   # Return true if lcx * x divides lcy * y
   _divides = function(lcx, x, lcy, y)
@@ -196,15 +197,12 @@ function _recombine(Ga, Gb; timings = Dict())
     return true
   end
 
-  polys_to_keep = Vector{Tuple{Int, Int}}()
-
-  _to_delete = Int[]
-
   _tmp = Vector{Int}(undef, Singular.nvars(S))
 
   @time for i in 1:length(Gagenslifted)
-    for j in 1:length(Gbgenslifted)
-      empty!(_to_delete)
+    j = 1
+    @label label1
+    while j < length(Gbgenslifted)
       Gai = Gagenslifted[i]
       Gbj = Gbgenslifted[j]
       _exp = _lcm_mon_exp!(_tmp, Gai, Gbj)
@@ -212,42 +210,36 @@ function _recombine(Ga, Gb; timings = Dict())
 
       redundant = false
       
-      for (_lc, e) in lt
+      for (p, _lc, e) in ltp
         if _divides(_lc, e, lcrecomb, _exp)
           redundant = true
-          break
+          j += 1
+          @goto label1
         end
       end
 
-      if redundant
-        continue
-      end
-
-      for k in 1:length(lt)
-        _lc, e = lt[k]
+      k = 1
+      while k <= length(ltp)
+        p, _lc, e = ltp[k]
         if _divides(lcrecomb, _exp, _lc, e)
-          push!(_to_delete, k)
+          deleteat!(ltp, k)
+          k -= 1
         end
+        k += 1
       end
-
-      if length(_to_delete) > 0
-        deleteat!(lt, _to_delete)
-        deleteat!(polys_to_keep, _to_delete)
-      end
-
-      push!(lt, (lcrecomb, copy(_exp)))
-      push!(polys_to_keep, (i, j))
+      push!(ltp, ((i,j), lcrecomb, copy(_exp)))
+      j += 1
     end
   end
 
-  resize!(new_polys, length(polys_to_keep))
   k = 1
-  for (i, j) in polys_to_keep
+  x = Singular.gens(S)
+  resize!(new_polys, length(ltp))
+  for ((i, j), _lc, e) in ltp
     Gai = Gagenslifted[i]
     Gbj = Gbgenslifted[j]
-    lmlcm = _lcm_mon(Singular.lm(Gai), Singular.lm(Gbj))
+    lmlcm = prod(x.^e)
     h =  ua * _div_mon(lmlcm, Singular.lm(Gbj)) * Singular.lc(Gai)::Singular.n_Zn * Gbj + vb * _div_mon(lmlcm, Singular.lm(Gai)) * Singular.lc(Gbj)::Singular.n_Zn * Gai
-#push!(new_polys, h)
     new_polys[k] = h
     k += 1
   end
