cab589a611367fa6a0ca6f32b2160c0b38aca790
Christian Eder <ederc@mathematik.uni-kl.de>
2019-09-16 11:32:53 +0200

    adds versioninfo function for GB

--- src/GB.jl
+++ src/GB.jl
@@ -9,6 +9,8 @@ using Libdl
 using Revise
 using Singular
 using Hecke
+using LibGit2
+using Dates
 
 export Singular
 export Hecke
@@ -548,5 +550,39 @@ function modf4(
             pbmfiles=pbmfiles, infolevel=infolevel, monorder=monorder)
     return res
 end
+
+function versioninfo()
+    print("GB v0.7\n")
+    gbrepo = dirname(dirname(@__FILE__))
+
+    print("GB: ")
+    prepo = LibGit2.GitRepo(gbrepo)
+    print("commit: ")
+    print(string(LibGit2.head_oid(prepo)))
+    print(" date: ")
+    commit = LibGit2.GitCommit(prepo, LibGit2.head_oid(prepo))
+    println(Dates.unix2datetime(LibGit2.author(commit).time))
+
+    finalize(prepo)
+
+    for deps in ["gb"]
+        if ispath(joinpath(gbrepo, "deps", deps))
+            print("$deps: ")
+            repo = joinpath(gbrepo, "deps", deps)
+
+            prepo = LibGit2.GitRepo(repo)
+
+            print("commit: ")
+            print(string(LibGit2.head_oid(prepo)))
+            print(" date: ")
+            commit = LibGit2.GitCommit(prepo, LibGit2.head_oid(prepo))
+            println(Dates.unix2datetime(LibGit2.author(commit).time))
+            finalize(prepo)
+        end
+    end
+
+return nothing
+end
+
 
 end # module
