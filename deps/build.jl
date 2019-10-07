717eb3e52d806dc592d40e6b2600b57ca220c91e
Christian Eder <ederc@mathematik.uni-kl.de>
2019-10-07 11:51:50 +0200

    adds GBBuilder to dependency build

--- deps/build.jl
+++ deps/build.jl
@@ -3,6 +3,34 @@ import Libdl
 
 oldwdir = pwd()
 
+issource_build = "GB_SOURCE_BUILD" in keys(ENV) && ENV["GB_SOURCE_BUILD"] == "1"
+
+if !issource_build
+  # Dependencies that must be installed before this package can be built
+  dependencies = [
+    # This has to be in sync with the corresponding commit in the source build below (for flint, arb, antic)
+    "https://github.com/JuliaPackaging/Yggdrasil/releases/download/GMP-v6.1.2-1/build_GMP.v6.1.2.jl",
+    "https://github.com/ederc/GBBuilder/releases/download/v0.13-dev2/build_libgb.v0.13.0-dev2.jl"
+   ]
+
+  const prefix = Prefix(get([a for a in ARGS if a != "--verbose"], 1, joinpath(@__DIR__, "usr")))
+
+  products = []
+
+  for url in dependencies
+      build_file = joinpath(@__DIR__, basename(url))
+      if !isfile(build_file)
+          download(url, build_file)
+      end
+  end
+
+  # Execute the build scripts for the dependencies in an isolated module to avoid overwriting
+  # any variables/constants here
+  for url in dependencies
+      build_file = joinpath(@__DIR__, basename(url))
+      m = @eval module $(gensym()); include($build_file); end
+      append!(products, m.products)
+  end
 # at the moment we only use the experimental plain branch of gb
 @show GB_BRANCH = "master"
 
