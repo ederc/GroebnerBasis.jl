1b2283c193519673ae8aebae8345a1dcf96f2c10
Christian Eder <ederc@mathematik.uni-kl.de>
2020-11-12 21:52:25 +0100

    adds dummy start for F5 algorithm

--- src/SignatureGB.jl
+++ src/SignatureGB.jl
@@ -1,3 +1,37 @@
+"""
+    f5(I[, nthrds::Int=1, maxpairs::Int=0, reducegb::Int=0,
+            infolevel::Int=0, monorder::Symbol=:degrevlex,
+            sigorder::Int=0])
+
+Compute a Groebner basis of the given ideal I w.r.t. to the given monomial
+order using Faugere's F4 algorithm. The function takes a Singular ideal as
+input and returns a Singular ideal. At the moment only finite fields up to
+31-bit and the rationals are supported as ground fields.
+
+# Arguments
+* `I::Singular.sideal`: ideal to compute a Groebner basis for.
+* `nthrds::Int=1`:  number of threads; default is 1.
+* `maxpairs::Int=0`:  maximal number of pairs selected for one matrix; default is
+                      0, i.e. no restriction. If matrices get too big or consume
+                      too much memory this is a good parameter to play with.
+* `reducegb::Int=0`:  reduce final basis; default is 0. Note that for
+                      computations over Q we do not normalize the polynomials,
+                      the basis is only minimal and tailreduced. Normalize by
+                      hand if necessary.
+* `infolevel::Int=0`: info level for printout:
+    - `0`: no printout (default),
+    - `1`:  a summary of the computational data is printed at the beginning and
+    the end of the computation,
+    - `2`: also dynamical information for each round resp. matrix is printed.
+* `monorder::Symbol=:degrevlex`: monomial order w.r.t. which the computation is
+                                done;
+    - `degrevlex`: the degree-reverse-lexicographical (DRL) order (default),
+    - `lex`: the lexicographical order (LEX).
+* `sigorder::Int=:0`: signature order w.r.t. which the computation is
+                                done;
+    - `0`: position-over-term order, like standard F5 (default),
+    - `1`: Schreyer order.
+"""
 function f5(
         I::Singular.sideal;           # input generators
         hts::Int=17,                  # hash table size, default 2^17
@@ -6,28 +40,49 @@ function f5(
                                       # in symbolic preprocessing
         reducegb::Int=0,              # reduce final basis
         infolevel::Int=0,             # info level for print outs
-        monorder::Symbol=:dregrevlex  # monomial order
+        monorder::Symbol=:dregrevlex, # monomial order
+        sigorder::Int=0               # signature order
         )
     R     = I.base_ring
     # skip zero generators in ideal
     ptr = Singular.libSingular.id_Copy(I.ptr, R.ptr)
     J   = Singular.Ideal(R, ptr)
     Singular.libSingular.idSkipZeroes(J.ptr)
-    # get number of variables
-    nvars   = Singular.nvars(R)
-    ngens   = Singular.ngens(J)
 
-    len = len_t(nvars)
-    print(len)
-    ord = 0
+    # initialize data structures
+    stat  = stat_t()
+    stat.numberVariables  = Singular.nvars(R)
+    stat.numberGenerators = Singular.ngens(J)
+
+    basis = basis_t()
+    basis.numberTerms   = Array{len_t}(undef, stat.numberGenerators)
+    basis.coefficients  = Array{Array{cf_t}}(undef, stat.numberGenerators)
+    basis.monomials     = Array{Array{Array{exp_t}}}(undef, stat.numberGenerators)
+
+    #= get monomial order =#
+    monomialOrder = 0
     if monorder == :degrevlex
-        ord = 0
+        monomialOrder = 0
     end
     if monorder == :lex
-        ord = 1
+        monomialOrder = 1
     end
-    char  = Singular.characteristic(R)
 
-    print(J)
-    print(char)
+    #= get signature order =#
+    signatureOrder  = sigorder
+
+    #= get field characteristic =#
+    stat.characteristic = Singular.characteristic(R)
+    if ! Nemo.isprime(Nemo.FlintZZ(stat.characteristic))
+        error("At the moment GroebnerBasis only supports finite fields and the rationals.")
+        return 0
+    end
+
+    #= store input generators data in basis =#
+    if 0 == stat.characteristic
+        #= to be implemented =#
+        #= convert_qq_singular_ideal_to_signature_basis(J, stat, basis) =#
+    else
+        convert_ff_singular_ideal_to_signature_basis(J, stat, basis)
+    end
 end
--- src/Typedefs.jl
+++ src/Typedefs.jl
@@ -1,1 +1,29 @@
 const len_t = UInt32
+const cf_t  = UInt32
+const pos_t = UInt32
+const deg_t = UInt16
+const exp_t = UInt16
+
+export stat_t, signature_t, basis_t
+
+mutable struct stat_t
+    numberVariables::Int
+    numberGenerators::Int
+    characteristic::Int
+    stat_t()  = new()
+end
+
+struct signature_t
+    monomial::Array{exp_t}
+    position::pos_t
+end
+
+mutable struct basis_t
+    load::len_t
+    maxDegree::deg_t
+    numberTerms::Array{len_t}
+    coefficients::Array{Array{cf_t}}
+    monomials::Array{Array{Array{exp_t}}}
+    signatures::Array{signature_t}
+    basis_t() = new()
+end
